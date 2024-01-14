79847a915a74849d6fc1741ddfaf8167561ccdbb
codehz <git@hertz.moe>
2024-01-14 23:32:38 +0800

    remove checksum log

--- src/struct/Font.ts
+++ src/struct/Font.ts
@@ -39,10 +39,6 @@ function calcTableChecksum(table: ArrayBuffer) {
   let sum = 0;
   for (let i = 0; i < arr.byteLength; i += 4) {
     sum = (sum + arr.getUint32(i)) % 0x1_00_00_00_00;
-    console.log(
-      sum.toString(16).padStart(8, "0"),
-      arr.getUint32(i).toString(16).padStart(8, "0")
-    );
   }
   return sum;
 }
